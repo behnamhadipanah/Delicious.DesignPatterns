ce13684174b2777542451c0f7b6f0ab8aa263b70
behnamhadipanah <behnamhadipanah@gmail.com>
2023-03-18 11:24:38 +0330

    Add WebClientDecorator

---
+++ Decorator/Example/Ex.WebClient/Ex.Decorator.WebClient.App/WebClientDecorator.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+
+namespace Ex.Decorator.App;
+
+public class WebClientDecorator : WebClient
+{
+    private readonly WebClient _webClient;
+
+    public WebClientDecorator(WebClient webClient)
+    {
+        _webClient = webClient;
+    }
+
+    public string DownloadString(string address)
+    {
+        if (address.StartsWith("https://github.com"))
+            return _webClient.DownloadString(address);
+        
+        return string.Empty;
+    }
+}
