722d476a6f8c90355914d3025dbb259515228ff6
Behnam Hadipanah <behnamhadipanah@outlook.com>
2024-07-31 14:43:24 +0330

    add composite pattern

---
+++ Composite/Composite_Pattern/CompositesClass/Component.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite_Pattern.CompositesClass
+{
+    public abstract class Component
+    {
+        protected string Name { get; set; }
+
+        public Component(string name)
+        {
+            Name = name;
+        }
+
+        public abstract void Add(Component component);
+        public abstract void Remove(Component component);
+        public abstract void Display(int depth);
+    }
+}
---
+++ Composite/Composite_Pattern/CompositesClass/Composite.cs
@@ -0,0 +1,36 @@
+﻿namespace Composite_Pattern.CompositesClass;
+
+public class Composite : Component
+{
+    private List<Component> _components = new List<Component>();
+
+    public Composite(string name) : base(name)
+    {
+    }
+
+    public Composite( string name, Component[] components):base(name)
+    {
+        foreach (var component in components)
+        {
+            Add(component);
+        }
+    }
+    public override void Add(Component component)
+    {
+        _components.Add(component);
+    }
+
+    public override void Remove(Component component)
+    {
+        _components.Remove(component);
+    }
+
+    public override void Display(int depth)
+    {
+        Console.WriteLine(new string('-', depth) + Name);
+        foreach (var component in _components)
+        {
+            component.Display(depth + 2);
+        }
+    }
+}
