1a4647f5fd14639b4820f99f0798fea51edc453b
b.hadipanah <b.hadipanah@adonistech.ir>
2024-08-18 13:30:36 +0330

    Add prototype project with doc

---
+++ ProtoType/Prototype_Pattern.App/ConcretePrototype1.cs
@@ -0,0 +1,17 @@
+﻿using Prototype_Pattern.App;
+
+public class ConcretePrototype1 : IPrototype
+{
+    public int Property1 { get; set; }
+    public string Property2 { get; set; }
+
+    public ConcretePrototype1(int property1, string property2)
+    {
+        Property1 = property1;
+        Property2 = property2;
+    }
+    public IPrototype Clone()
+    {
+        return (IPrototype)this.MemberwiseClone();
+    }
+}
