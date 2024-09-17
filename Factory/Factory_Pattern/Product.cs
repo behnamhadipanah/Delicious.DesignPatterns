a3e564fcec7058c6fcc51876edee9905c961d6f5
b.hadipanah <b.hadipanah@adonistech.ir>
2024-09-17 15:12:44 +0330

    added Factory Pattern console app project

---
+++ Factory/Factory_Pattern/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Factory_Pattern.ConcreteProducts;
+
+namespace Factory_Pattern;
+public abstract class Product //for example send email
+{
+    public abstract void Execute();
+}
+
+public abstract class CreatorFactory
+{
+    public abstract Product FactoryMethod(int i);
+}
+
+public class ConcreteCreatorFactory : CreatorFactory
+{
+    public override Product FactoryMethod(int i)
+    {
+        if (i < 0)
+            return new ConcreteProduct1();
+
+        else  if (i > 0)
+            return new ConcreteProduct2();
+        else
+        {
+            return new DefaultProduct();
+        }
+
+
+    }
+}
