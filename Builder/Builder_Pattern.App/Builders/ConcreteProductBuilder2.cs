38b3b15c8f57adbf2c81bdc3cc90a2f5e5540a9d
Behnam Hadipanah <behnamhadipanah@outlook.com>
2024-09-26 12:15:28 +0330

    Added builder pattern project with document

---
+++ Builder/Builder_Pattern.App/Builders/ConcreteProductBuilder2.cs
@@ -0,0 +1,24 @@
+﻿using Builder_Pattern.App.Entities;
+
+namespace Builder_Pattern.App.Builders;
+
+public class ConcreteProductBuilder2 : ProductBuilder
+{
+    public override void BuildPart1()
+    {
+        _product.Part1 = "x Part-1";
+    }
+
+    public override void BuildPart2()
+    {
+        _product.Part2 = "x Part-2";
+
+    }
+
+    public override Product GetResult()
+    {
+        _product.Name = "Product 2";
+        Console.WriteLine($"{_product.Name} created");
+        return base.GetResult();
+    }
+}
---
+++ Builder/Builder_Pattern.App/Builders/Director.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder_Pattern.App.Builders
+{
+    public class Director
+    {
+        private ProductBuilder _builder;
+
+        public void SetBuilder(ProductBuilder builder)
+        {
+            _builder = builder;
+        }
+
+        public void Construct()
+        {
+            _builder.BuildPart1();
+            _builder.BuildPart2();
+        }
+    }
+}
