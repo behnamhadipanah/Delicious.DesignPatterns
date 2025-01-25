9e3aeb5e703a8e7d188f122cc461568016b1af47
Behnam Hadipanah <behnamhadipanah@outlook.com>
2025-01-25 13:10:39 +0330

    added observer pattern with doc and uml

---
+++ Observer/Observer_Pattern/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+ConcreteSubject concreteSubject = new ConcreteSubject();
+concreteSubject.Attach(new ConcreteObserver("observer 1",concreteSubject));
+concreteSubject.Attach(new ConcreteObserver("observer 2", concreteSubject));
+concreteSubject.Attach(new ConcreteObserver("observer 3", concreteSubject));
+
+
+var observer4 = new ConcreteObserver("observer 4", concreteSubject);
+concreteSubject.Attach(observer4);
+
+
+// Apply change on concrete subject
+
+
+concreteSubject.SubjectState = "Apply change to state";
+
+concreteSubject.Notify();
+concreteSubject.Detach(observer4);
+
+
+concreteSubject.SubjectState = "Apply again change to state";
+
+concreteSubject.Notify();
+
+Console.ReadKey();
