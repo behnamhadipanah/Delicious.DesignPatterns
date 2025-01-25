b77aa768c2f3441845a723880d5fceb7e2813320
Behnam Hadipanah <behnamhadipanah@outlook.com>
2025-01-25 11:33:19 +0330

    added memento project with doc and uml

---
+++ Memento/Memento_Pattern/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Memento_Pattern;
+
+Console.WriteLine("Hello, World!");
+
+
+Originator originator = new Originator();
+
+originator.State = "Blue";
+
+Caretaker caretaker = new Caretaker();
+
+caretaker.Memento=originator.CreateMemento();
+
+originator.State = "Green";
+originator.State = "Yellow";
+originator.State = "DarkRed";
+
+originator.SetMemento(caretaker.Memento);
+
+Console.ReadKey();
+
+
